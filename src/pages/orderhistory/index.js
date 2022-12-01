74d1292530636067e032a6d28ec666b61e43ad9f
MoIkrom <mikram216@gmail.com>
2022-12-02 01:04:45 +0700

    added page Order History

---
+++ src/pages/orderhistory/index.js
@@ -0,0 +1,152 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from "react";
+
+import styles from "../../styles/orderhistory.module.css";
+import Image from "next/image";
+
+// Import React-Bootsrap
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
+
+// Import Component
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+// Import Image
+import dot from "../../assets/images/dot.png";
+import profile from "../../assets/images/jonas.png";
+import card from "../../assets/images/card.png";
+import star from "../../assets/images/star.png";
+import Moviegoers from "../../assets/images/Moviegoers.png";
+import points from "../../assets/images/points.png";
+import tigaduanol from "../../assets/images/tigaduanol.png";
+import loading from "../../assets/images/fill-loading.png";
+import nonloading from "../../assets/images/nonloading.png";
+import cineone from "../../assets/images/cineone.png";
+import chevrondown from "../../assets/images/chevrondown.png";
+import ebu from "../../assets/images/ebu.png";
+
+function index() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <Navbar />
+      <main className={` container-fluid ${styles["cont-fluid"]}`}>
+        <div className={`container d-flex justify-content-between ${styles["cont-up"]} `}>
+          <div className={` card col-lg-3 ${styles["card-profile"]}`}>
+            <div className={`container px-4 `}>
+              <div className={`d-flex justify-content-between pt-4`}>
+                <p className={styles.info}>INFO </p>
+                <Image src={dot} alt="/" />
+              </div>
+              <div className={` justify-content-center align-items-center pt-5 ${styles["cont-profile"]}`}>
+                <Image className="mb-3" src={profile} alt="/" width={136} height={136} />
+                <p className={`mb-0 ${styles.jonas}`}>Jonas El Rodriguez</p>
+                <p className={`${styles.MoviegoersText}`}>Moviegoers </p>
+              </div>
+            </div>
+            <hr className="mt-4" />
+            <div className="container px-4 pb-5">
+              <p>Loyalty Points</p>
+              <div>
+                <Image className={`${styles["card"]}`} src={card} alt="/" />
+                <Image className={`${styles["points"]}`} src={points} alt="/" />
+                <Image className={`${styles["Moviegoers"]}`} src={Moviegoers} alt="/" />
+                <Image className={`${styles["star"]}`} src={star} alt="/" />
+                <Image className={`${styles["tigaduanol"]}`} src={tigaduanol} alt="/" />
+              </div>
+              <p>180 points become a master</p>
+              <div>
+                <Image className={`${styles["non-loadings"]}`} src={nonloading} alt="/" />
+                <Image className={`${styles["loadings"]}`} src={loading} alt="/" />
+              </div>
+            </div>
+          </div>
+          <div className={`d-flex  col-lg-8 ${styles.contRight} `}>
+            <div className={` card  d-flex justify-content-center ${styles["bar"]}`}>
+              <div className={` d-flex gap-5 ps-5 ${styles["nav-account-set"]}`}>
+                <p className={`mb-0 ${styles.account} ${styles.cursor}`}>Account Settings </p>
+                <p className={`mb-0 ${styles.order} ${styles.cursor}`}>Order History </p>
+              </div>
+            </div>
+            <div className={`card d-flex pt-4 justify-content-center ${styles["hist"]}`}>
+              <div className={` d-flex px-5 ${styles["history-ticket"]}`}>
+                <p className={`mb-0 ${styles.date}`}>Tuesday, 07 July 2020 - 04:30pm </p>
+                <div className={`d-flex justify-content-between`}>
+                  <p className={`mb-0 ${styles.homecoming}`}>Spider-Man: Homecoming </p>
+                  <Image src={cineone} alt="/" />
+                </div>
+              </div>
+              <hr />
+              <div className={`container d-flex justify-content-between ${styles.collapse}`}>
+                <div className={` card ${styles.ticketz} ${styles.cursor} `}>Ticket in active</div>
+                <div className={` ${styles.cursor}`} onClick={() => setOpen(!open)} aria-controls="example-collapse-text" aria-expanded={open}>
+                  <div className="d-flex gap-3 align-items-center">
+                    <p className="mb-0">Show Details</p>
+                    <Image src={chevrondown} alt="/" />
+                  </div>
+                </div>
+              </div>
+              <Collapse in={open}>
+                <div className="container px-5 " id="example-collapse-text">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                </div>
+              </Collapse>
+            </div>
+            <div className={`card d-flex pt-4 justify-content-center ${styles["hist"]}`}>
+              <div className={` d-flex px-5 ${styles["history-ticket"]}`}>
+                <p className={`mb-0 ${styles.date}`}>Monday, 14 June 2020 - 02:00pm </p>
+                <div className={`d-flex justify-content-between`}>
+                  <p className={`mb-0 ${styles.homecoming}`}>Avengers: End Game </p>
+                  <Image src={ebu} alt="/" />
+                </div>
+              </div>
+              <hr />
+              <div className={`container d-flex justify-content-between ${styles.collapse}`}>
+                <div className={` card ${styles.ticketz} ${styles.cursor} ${styles.used} `}>Ticket used</div>
+                <div className={` ${styles.cursor}`} onClick={() => setOpen(!open)} aria-controls="example-collapse-text1" aria-expanded={open}>
+                  <div className="d-flex gap-3 align-items-center">
+                    <p className="mb-0">Show Details</p>
+                    <Image src={chevrondown} alt="/" />
+                  </div>
+                </div>
+              </div>
+              <Collapse in={open}>
+                <div className="container px-5 " id="example-collapse-text">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                </div>
+              </Collapse>
+            </div>
+            <div className={`card d-flex pt-4 justify-content-center ${styles["hist"]}`}>
+              <div className={` d-flex px-5 ${styles["history-ticket"]}`}>
+                <p className={`mb-0 ${styles.date}`}>Monday, 10 March 2020 - 04:00pm </p>
+                <div className={`d-flex justify-content-between`}>
+                  <p className={`mb-0 ${styles.homecoming} `}>Thor: Ragnarok</p>
+                  <Image src={ebu} alt="/" />
+                </div>
+              </div>
+              <hr />
+              <div className={`container d-flex justify-content-between ${styles.collapse}`}>
+                <div className={` card ${styles.ticketz} ${styles.cursor} ${styles.used}`}>Ticket used</div>
+                <div className={` ${styles.cursor}`} onClick={() => setOpen(!open)} aria-controls="example-collapse-text" aria-expanded={open}>
+                  <div className="d-flex gap-3 align-items-center">
+                    <p className="mb-0">Show Details</p>
+                    <Image src={chevrondown} alt="/" />
+                  </div>
+                </div>
+              </div>
+              <Collapse in={open}>
+                <div className="container px-5 " id="example-collapse-text">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                </div>
+              </Collapse>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default index;
