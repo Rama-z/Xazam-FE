bd5682ad8fc993d83ace0b219d51f7351b5d1c4e
Zanuar Bagus Ramadhan <zanuarbagusramadhan@gmail.com>
2022-12-04 16:18:28 +0700

    movie

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
   async redirects() {
     return [
       {
