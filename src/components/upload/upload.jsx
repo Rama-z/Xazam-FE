b1b63ab327aed690739537155fe60cb31bab5115
Zanuar Bagus Ramadhan <zanuarbagusramadhan@gmail.com>
2022-12-04 16:17:36 +0700

    movie

--- src/components/upload/upload.jsx
+++ src/components/upload/upload.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 import styles from "../upload/upload.module.css";
+import Image from "next/image";
 
 const Upload = ({ img, ...rest }) => {
   return (
     <div className={`${styles["upload"]}`}>
       {img && (
-        <img className={`${styles["img-preview"]}`} src={img} alt="preview" />
+        <Image className={`${styles["img-preview"]}`} src={img} alt="preview" />
       )}{" "}
       <br />
       <label for="file-upload" className={`${styles["custom-file-upload"]}`}>
-        <i className={`${styles["theButton"]}`}>✎</i> 
+        <i className={`${styles["theButton"]}`}>✎</i>
       </label>
       <input
         name="image"
