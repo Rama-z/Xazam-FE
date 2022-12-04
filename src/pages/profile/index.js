5557ebdc59dbfed975450675629c37a05b6f3d2e
luqmanito <luqmangrahito@gmail.com>
2022-12-04 21:37:01 +0700

    fixingStyleResponsive

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -107,6 +107,7 @@ const Profile = () => {
     }
   }
 
+  console.log(data);
   useEffect(() => {
     console.log(body);
   }, [body]);
@@ -189,7 +190,7 @@ const Profile = () => {
                   </div>
                   <div class="col"></div>
                 </div>
-                <hr />
+                <hr className={` ${styles["hr"]}`} /> 
                 <p className={` ${styles["details"]}`}>Details Information</p>
                 <p onClick={handleChange} className={` ${styles["edit"]}`}>
                   ✎ Edit
@@ -267,13 +268,15 @@ const Profile = () => {
                     />
                   </div>
                 </form>
+                <p className={` ${styles["privacy"]}`}>
+                        Account and Privacy
+                      </p>
+                      <hr className={` ${styles["hr"]}`} />
                 <form className="col" onSubmit={changePwdSubmitHandler}>
                   <div className="row">
                     <div className="col">
-                      <p className={` ${styles["privacy"]}`}>
-                        Account and Privacy
-                      </p>
-                      <hr className={` ${styles["hr-1"]}`} />
+                      
+                      {/* <hr className={` ${styles["hr-1"]}`} /> */}
                       <p className={` ${styles["pass"]}`}>Old Password</p>
                       <p
                         onClick={handleChange2}
@@ -302,7 +305,7 @@ const Profile = () => {
                       />
                     </div>
                     <div className="col">
-                      <hr className={` ${styles["hr-2"]}`} />
+                      {/* <hr className={` ${styles["hr-2"]}`} /> */}
                       <p className={` ${styles["confirm-pw"]}`}>New Password</p>
                       <input
                         onChange={changePwdHandler}
