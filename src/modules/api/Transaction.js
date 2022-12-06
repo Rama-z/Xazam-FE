16b31d904aeb494999959da2dee06428dee64ce3
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-06 08:52:57 +0700

    Updated files

--- src/modules/api/Transaction.js
+++ src/modules/api/Transaction.js
@@ -3,18 +3,6 @@ import Axios from "axios";
 const config = (token) => {
   return {
     headers: {
-<<<<<<< HEAD
-      authorization: `Bearer ${token}`
-    },
-  }
-}
-
-const BASE_URL = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/xazam/transaction`;
-
-const gethistory = (token) => Axios.get(`${BASE_URL}/history`, config(token));
-
-export { gethistory };
-=======
       authorization: `Bearer ${token}`,
     },
   };
@@ -27,4 +15,3 @@ const createTrans = (data, token) =>
 const gethistory = (token) => Axios.get(`${BASE_URL}/history`, config(token));
 
 export { gethistory, createTrans };
->>>>>>> 43055339896278aee4cd635e89bcf7324ec28514
