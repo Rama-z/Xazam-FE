103998e3b0e5c935b55eeda90f664271ffa991d3
MUH. AHSAN <muh.ahsan.student@gmail.com>
2022-12-06 08:52:19 +0700

    Updated files

--- src/redux/actions/transaction.js
+++ src/redux/actions/transaction.js
@@ -1,54 +1,13 @@
 import { ActionType } from "redux-promise-middleware";
 
-<<<<<<< HEAD
-import { gethistory } from "../../modules/api/Transaction";
-
-import { actionTransactions } from "./actionStrings";
-=======
 import { gethistory, createTrans } from "src/modules/api/Transaction";
 
 import { actionStrings, actionTransactions } from "./actionStrings";
->>>>>>> 43055339896278aee4cd635e89bcf7324ec28514
 
 const { Pending, Rejected, Fulfilled } = ActionType;
 
 const getHistoryPending = () => ({
   type: actionTransactions.getHistory.concat("-", Pending),
-<<<<<<< HEAD
-})
-
-const getHistoryRejected = (error) => ({
-  type: actionTransactions.getHistory.concat("-", Rejected),
-  payload: {error},
-})
-
-const getHistoryFulfilled = (data) => ({
-  type: actionTransactions.getHistory.concat("-", Fulfilled),
-  payload: {data},
-})
-
-const getHistoryThunk = (token, success, denied) => {
-  return async(dispatch) => {
-    try {
-      dispatch(getHistoryPending());
-      const result = await gethistory(token);
-      console.log(result.data);
-      dispatch(getHistoryFulfilled(result.data));
-      typeof success === "function" && success();
-    } catch (error) {
-      dispatch(getHistoryRejected(error));
-      typeof denied === "function" && denied();
-    }
-  }
-}
-
-
-const transactionAction = {
-  getHistoryThunk,
-}
-
-export default transactionAction;
-=======
 });
 
 const getHistoryRejected = (error) => ({
@@ -107,4 +66,3 @@ const transactionAction = {
 };
 
 export default transactionAction;
->>>>>>> 43055339896278aee4cd635e89bcf7324ec28514
